da601669831b2444f2224456d879c01638795060
Gautham Vanjre <vanjregautham@gmail.com>
2022-12-03 21:28:40 +0530

    completed navbar for mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../images/logo.svg";
+import hamburger from "../images/icon-hamburger.svg";
 
 const Navbar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   return (
     <>
       <nav>
         <div>
           <img src={logo} alt="logo" />
         </div>
-        <div>
+        <div
+          className={
+            isNavExpanded
+              ? "nav-links-container nav-links-parent"
+              : "nav-links-container"
+          }
+        >
           <ul className="nav-links">
             <li>Home</li>
             <li>About</li>
@@ -18,7 +27,16 @@ const Navbar = () => {
             <li>Careers</li>
           </ul>
         </div>
-        <div>
+        <div className="hamburger-icon">
+          <button
+            onClick={() => {
+              setIsNavExpanded(!isNavExpanded);
+            }}
+          >
+            <img src={hamburger} alt="" />
+          </button>
+        </div>
+        <div className="nav-links-container">
           <button className="invite-btn">Request Invite</button>
         </div>
       </nav>
